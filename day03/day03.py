5e5c848f8001bd0fd5bc042572b0b64d1d0a0cc3
Mariano Cerdeiro <mcerdeiro@gmail.com>
2020-12-03 06:41:07 +0100

    day3

---
+++ day03/day03.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import itertools
+from functools import reduce
+from collections import defaultdict
+
+lines = open("day03.dat", "r").read().splitlines()
+
+def get(l, p):
+  if (p == int(p)):
+    p = int(p)
+  else:
+    return "."
+  p = (p % len(l))
+  return l[p]
+
+ans1 = 0
+ans2 = 0
+
+pos = 0
+slope = [1, 3, 5, 7, 0.5]
+r = []
+
+for s in slope:
+  pos = 0
+  ans1 = 0
+  for line in lines:
+     if (get(line,pos)=="#"):
+        ans1 += 1
+     pos += s
+  r.append(ans1)
+
+ans1 = r[1]
+ans2 = reduce(lambda a,b: a*b, r)
+
+
+print ("Part1: " + str(ans1))
+print ("Part2: " + str(ans2))
+
